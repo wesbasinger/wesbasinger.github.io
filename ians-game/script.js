857623d1abca36ff0fd50092a88dfac1df7a4ddd
Wes Basinger <wbasinger@villagetechschools.org>
2017-12-29 14:32:15 -0600

    Added an export view to the workday app and forcing a refresh on return to home click.

--- ians-game/script.js
+++ ians-game/script.js
@@ -1,20 +1,73 @@
 var canvas = document.getElementById("FRAME");
 var c = canvas.getContext('2d');
+var A_PRESSED = false;
+var D_PRESSED = false;
+var SPACE_PRESSED = false;
+var A = 65;
+var D = 68;
+var SPACE = 32;
+var platformFill = "green";
 
-console.log(canvas)
-console.log(c)
+document.addEventListener("keydown", keyDownHandler);
+document.addEventListener("keyup", keyUpHandler);
+
+// the keyDownHandler and the keyUpHandler is for seeing if the player presses some keys, and if they do a boolean changes.
+function keyDownHandler(e) {
+  if (e.keyCode == A) {
+    A_PRESSED = true;
+  }
+  else if (e.keyCode == D) {
+    D_PRESSED = true;
+  }
+  else if (e.keyCode == SPACE) {
+    SPACE_PRESSED = true;
+  }
+}
+
+// the keyDownHandler and the keyUpHandler is for seeing if the player presses some keys, and if they do a boolean changes.
+function keyUpHandler(e) {
+  if (e.keyCode == A) {
+    A_PRESSED = false;
+  }
+  else if (e.keyCode == D) {
+    D_PRESSED = false;
+  }
+  else if (e.keyCode == SPACE) {
+    SPACE_PRESSED = false;
+  }
+}
+
 
 var player = {
-    x : 10,          // I fixed that. Sorry, I'm a noobie at the 'this' keyword
-    y : 10,
-    width : 10, // not sure why these need to be properties on the object, are they going to change?
-    // No, I just put them there because I like it neat.
-    height : 10, // not sure why these need to be properties on the object, are they going to change?
+    x : 30,
+    y : 380,
+    width : 20,
+    height : 20,
     color : "blue",
-    make : function() {
+    Make : function() {
       c.fillStyle = this.color;
       c.fillRect(this.x, this.y, this.width, this.height);
     }
 }
 
-player.make()
+// This function makes the platforms that the player can jump on.
+function platforms() {
+  c.beginPath();
+  // Make all platforms under this comment!
+  c.rect(0, 325, 150, 20);
+  // Make all platforms above this comment!
+  c.fillStyle = platformFill;
+  c.fill();
+  c.closePath();
+}
+
+// This function has all of the animation inside of it.
+function draw() {
+  platforms();
+  player.Make()
+}
+
+// https://wesbasinger.github.io/ians-game/index.html
+
+// Makes the animation happen
+requestAnimationFrame(draw)
